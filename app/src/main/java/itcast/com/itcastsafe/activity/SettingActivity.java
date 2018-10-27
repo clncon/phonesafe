ff864c3fe95f08cad8771ee8ab2b0ab6bf49979a
风居住过的街道 <clncon@163.com>
2018-10-27 21:01:59 +0800

    use custom_view replace system_view

--- app/src/main/java/itcast/com/itcastsafe/activity/SettingActivity.java
+++ app/src/main/java/itcast/com/itcastsafe/activity/SettingActivity.java
@@ -1,36 +1,44 @@
 package itcast.com.itcastsafe.activity;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 import itcast.com.itcastsafe.R;
+import itcast.com.itcastsafe.activity.view.SettingItemView;
 
 public class SettingActivity extends Activity {
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        final SettingItemView settingItemView = findViewById(R.id.sv_update);
+        settingItemView.setTv_title("自动更新设置");
+        settingItemView.setTv_desc("自动更新设置已经关闭");
+        SharedPreferences sharedPreferences = getSharedPreferences("config",MODE_PRIVATE);
+
+        settingItemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                   //判断是否选中
+                   if(settingItemView.isChecked()){
+                        settingItemView.setIsChecked(false);
+                        settingItemView.setTv_desc("自动更新设置已经关闭");
+                   }else{
+                       settingItemView.setIsChecked(true);
+                       settingItemView.setTv_desc("自动更新设置已经开启");
+                   }
+            }
+        });
+
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.setting, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+   
+
 }
--- app/src/main/java/itcast/com/itcastsafe/activity/view/SettingItemView.java
+++ app/src/main/java/itcast/com/itcastsafe/activity/view/SettingItemView.java
@@ -3,7 +3,12 @@ package itcast.com.itcastsafe.activity.view;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.View;
+import android.widget.CheckBox;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+import itcast.com.itcastsafe.R;
+import itcast.com.itcastsafe.activity.SettingActivity;
 
 /**
  * @Author:MingKong
@@ -13,19 +18,54 @@ import android.widget.RelativeLayout;
  */
 @TargetApi(21)
 public class SettingItemView extends RelativeLayout{
+    private  TextView tv_title;
+    private  TextView tv_desc;
+    private CheckBox checkBox;
     public SettingItemView(Context context) {
         super(context);
+        initView();
     }
 
     public SettingItemView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        initView();
     }
 
     public SettingItemView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        initView();
     }
 
     public SettingItemView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+
         super(context, attrs, defStyleAttr, defStyleRes);
+        initView();
+    }
+
+    /**
+     * 初始化布局
+     */
+    private void initView(){
+        //将自定义好的布局文件设置给当前的SettingItemView
+        View setting_item = View.inflate(getContext(), R.layout.view_setting_item,this);
+        checkBox  = setting_item.findViewById(R.id.cb_status);
+        tv_title = setting_item.findViewById(R.id.tv_title);
+        tv_desc = setting_item.findViewById(R.id.tv_desc);
+    }
+
+    public void setTv_title(String title){
+     tv_title.setText(title);
+    }
+
+    public void setTv_desc(String desc){
+        tv_desc.setText(desc);
+    }
+
+    public boolean isChecked() {
+        return checkBox.isChecked();
+    }
+
+    public void setIsChecked(boolean flag) {
+        this.checkBox.setChecked(flag);
     }
 }
