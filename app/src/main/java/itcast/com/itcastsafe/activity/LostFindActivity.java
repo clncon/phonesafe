f5e65167094a045c10ed6c6f5cb0a69443839a74
风居住过的街道 <clncon@163.com>
2018-11-05 02:38:24 +0800

    add dynamic phone protect page

--- app/src/main/java/itcast/com/itcastsafe/activity/LostFindActivity.java
+++ app/src/main/java/itcast/com/itcastsafe/activity/LostFindActivity.java
@@ -7,21 +7,37 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import itcast.com.itcastsafe.R;
 
 public class LostFindActivity extends Activity {
 
     private SharedPreferences config;
+    private ImageView iv_protect;
+    private TextView tv_phone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         config = getSharedPreferences("config", MODE_PRIVATE);
         boolean configed = config.getBoolean("configed",false);
         if(configed){
             //设置过手机防盗
             setContentView(R.layout.activity_lost_find);
+            iv_protect = findViewById(R.id.iv_protect);
+            tv_phone = findViewById(R.id.tv_phone);
+            //从shareConifg中获取安全号码和设置防盗保护打开
+            boolean protect = config.getBoolean("protect", false);//是否保护开启
+            String phone = config.getString("phone", "");//安全号码
+            tv_phone.setText(phone);
+            if(protect){
+                iv_protect.setImageResource(R.mipmap.lock);
+            }else{
+
+                iv_protect.setImageResource(R.mipmap.unlock);
+            }
         }else{
 
             //没有设置过手机防盗
@@ -29,6 +45,8 @@ public class LostFindActivity extends Activity {
             finish();
 
         }
+
+
     }
 
     public void reEnter(View v){
